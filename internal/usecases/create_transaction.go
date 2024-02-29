434b7d7ac95e9055b8ed3578c9be731799cfa7a2
rafael-truepay <rafael.escobar@tino.com.br>
2024-02-28 21:12:34 -0300

    Adding use cases CreateAccount, GetAccount and CreateTransaction

--- internal/usecases/create_transaction.go
+++ internal/usecases/create_transaction.go
@@ -5,24 +5,58 @@ import (
 
 	"github.com/transaction-manager/internal/domain"
 	"github.com/transaction-manager/internal/usecases/ports"
+	"go.uber.org/zap"
 )
 
 type CreateTransactionUseCase struct {
-	TransactionRepository ports.TransactionRepository
-	AccountRepository     ports.AccountRepository
+	TransactionRepository   ports.TransactionRepository
+	AccountRepository       ports.AccountRepository
+	OperationTypeRepository ports.OperationTypeRepository
+	logger                  *zap.Logger
 }
 
 func NewCreateTransactionUseCase(
 	transactionRepository ports.TransactionRepository,
 	accountRepository ports.AccountRepository,
+	operationTypeRepository ports.OperationTypeRepository,
+	logger *zap.Logger,
 ) *CreateTransactionUseCase {
 	return &CreateTransactionUseCase{
-		TransactionRepository: transactionRepository,
-		AccountRepository:     accountRepository,
+		TransactionRepository:   transactionRepository,
+		AccountRepository:       accountRepository,
+		OperationTypeRepository: operationTypeRepository,
+		logger:                  logger,
 	}
 }
 
-func (*CreateTransactionUseCase) Run(ctx context.Context, transaction domain.Transaction) (error, int) {
+func (c *CreateTransactionUseCase) Run(ctx context.Context, transaction *domain.Transaction) (error, int64) {
+	c.logger.Info("[CreateTransactionUseCase] starting", zap.Any("transaction", transaction))
 
-	return nil, 0
+	account, err := c.AccountRepository.FindByID(transaction.AccountID)
+	if err != nil {
+		c.logger.Error("[CreateTransactionUseCase] error finding account by ID", zap.Error(err))
+		return err, 0
+	}
+	if account == nil {
+		c.logger.Info("[CreateTransactionUseCase] account not found", zap.Any("accountID", transaction.AccountID))
+		return domain.ErrIncorrectAccount, 0
+	}
+
+	operationType, err := c.OperationTypeRepository.FindByID(transaction.OperationTypeID)
+	if err != nil {
+		c.logger.Error("[CreateTransactionUseCase] error finding operation type by ID", zap.Error(err))
+		return err, 0
+	}
+	if operationType == nil {
+		c.logger.Info("[CreateTransactionUseCase] operation type not found", zap.Any("operationTypeID", transaction.OperationTypeID))
+		return domain.ErrIncorrectOperationType, 0
+	}
+
+	transactionID, err := c.TransactionRepository.Create(transaction)
+	if err != nil {
+		c.logger.Error("[CreateTransactionUseCase] error creating transaction", zap.Error(err))
+		return err, 0
+	}
+	c.logger.Info("[CreateTransactionUseCase] transaction created", zap.Any("transactionID", transactionID))
+	return nil, transactionID
 }
--- internal/usecases/get_acount.go
+++ internal/usecases/get_acount.go
@@ -4,15 +4,36 @@ import (
 	"context"
 
 	"github.com/transaction-manager/internal/domain"
+	"github.com/transaction-manager/internal/usecases/ports"
+	"go.uber.org/zap"
 )
 
-type GetAccountUseCase struct{}
-
-func NewGetAccountUseCase() *GetAccountUseCase {
-	return &GetAccountUseCase{}
+type GetAccountUseCase struct {
+	AccountRepository ports.AccountRepository
+	logger            *zap.Logger
 }
 
-func (*GetAccountUseCase) Run(ctx context.Context, accountID int) (error, domain.Account) {
+func NewGetAccountUseCase(
+	accountRepository ports.AccountRepository,
+	logger *zap.Logger,
+) *GetAccountUseCase {
+	return &GetAccountUseCase{
+		AccountRepository: accountRepository,
+		logger:            logger,
+	}
+}
 
-	return nil, domain.Account{}
+func (g *GetAccountUseCase) Run(ctx context.Context, accountID int64) (error, *domain.Account) {
+	g.logger.Info("[GetAccountUseCase] starting", zap.Any("accountID", accountID))
+	account, err := g.AccountRepository.FindByID(accountID)
+	if err != nil {
+		g.logger.Error("[GetAccountUseCase] error finding account by ID", zap.Error(err))
+		return err, nil
+	}
+	if account == nil {
+		g.logger.Info("[GetAccountUseCase] account not found", zap.Any("accountID", accountID))
+		return domain.ErrAccountNotFound, nil
+	}
+	g.logger.Info("[GetAccountUseCase] account found", zap.Any("account", account))
+	return nil, account
 }
