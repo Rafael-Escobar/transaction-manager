4d6897b88abe4c2a111f7cf4c181758a39a25b6e
rafael-truepay <rafael.escobar@tino.com.br>
2024-02-29 07:41:52 -0300

    Adding rule for document number

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -1,13 +1,22 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/klassmann/cpfcnpj"
+)
 
 var (
-	ErrAccountNotFound      = errors.New("account not found")
-	ErrAccountAlreadyExists = errors.New("account already exists")
+	ErrAccountNotFound       = errors.New("account not found")
+	ErrAccountAlreadyExists  = errors.New("account already exists")
+	ErrInvalidDocumentNumber = errors.New("invalid document number")
 )
 
 type Account struct {
 	ID             int64  `db:"id"`
 	DocumentNumber string `db:"document_number"`
+}
+
+func (a *Account) IsDocumentNumberValid() bool {
+	return cpfcnpj.ValidateCNPJ(a.DocumentNumber) || cpfcnpj.ValidateCPF(a.DocumentNumber)
 }
