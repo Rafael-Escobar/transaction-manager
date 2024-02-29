0ea25bb3109c4d66511d7bc8eef3620128f56e67
rafael-truepay <rafael.escobar@tino.com.br>
2024-02-29 20:12:28 -0300

    Increase test for create account use case

--- internal/usecases/create_account_test.go
+++ internal/usecases/create_account_test.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"context"
+	"database/sql"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -38,6 +39,14 @@ var _ = Describe("CreateAccountUseCase", func() {
 		}
 	})
 	Context("CreateAccountUseCase", func() {
+		When("Fail on verify account ID is from an existent account", func() {
+			It("Returns an error", func() {
+				accountRepository.On("FindByDocumentNumber", validDocumentNumber).Return(nil, sql.ErrConnDone)
+				accountID, err := useCase.Run(context.Background(), validAccount)
+				Expect(accountID).To(BeZero())
+				Expect(err).To(HaveOccurred())
+			})
+		})
 		When("The account ID is from one existent account", func() {
 			It("Returns a domain error ErrAccountAlreadyExists", func() {
 				accountRepository.On("FindByDocumentNumber", validDocumentNumber).Return(validAccount, nil)
@@ -55,6 +64,15 @@ var _ = Describe("CreateAccountUseCase", func() {
 				accountID, err := useCase.Run(context.Background(), validAccount)
 				Expect(accountID).ToNot(BeZero())
 				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+		When("The account ID are not related to a account, but fail on save.", func() {
+			It("Returns an error", func() {
+				accountRepository.On("FindByDocumentNumber", validDocumentNumber).Return(nil, nil)
+				accountRepository.On("Create", validAccount).Return(int64(0), sql.ErrConnDone)
+				accountID, err := useCase.Run(context.Background(), validAccount)
+				Expect(accountID).To(BeZero())
+				Expect(err).To(HaveOccurred())
 			})
 		})
 
